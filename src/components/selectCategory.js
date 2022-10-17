68c1e2cf426f86a4ef2b322461122d88c9cc12c2
glory <aremuopomulero@gmail.com>
2022-10-17 13:04:12 +0100

    changes

--- src/components/selectCategory.js
+++ src/components/selectCategory.js
@@ -18,7 +18,7 @@ function SelectCategory({ formFields,setFormData}) {
                 setCurrentCategory(category);
             });
         }
-    },[currentCategory,])
+    },[currentCategory,setCurrentCategory])
 
     const getCategories =useCallback(()=>{
         get(`${CATEGORYLISTURL}?parent=${currentCategory}&children=true`,{
@@ -27,23 +27,27 @@ function SelectCategory({ formFields,setFormData}) {
             const {status,categories}= resp.data;
             if(status){
                 if(categories.length > 0){
-                    const newData = [...data,categories];
-                    setData(newData);
-                    sessionStorage.setItem('loadedCategories',JSON.stringify(newData))
+                    // const newData = [...data,categories];
+                    setData(data =>{
+                        const newData = [...data,categories];
+                        sessionStorage.setItem('loadedCategories',JSON.stringify(newData))
+                      return newData;
+                    } );
+
                 }
             }
                 loadIncomingCategories();
         }).catch(e=>{
 
         });
-    },[currentCategory,data,formFields,loadIncomingCategories]);
+    },[currentCategory,formFields,loadIncomingCategories]);
 
     const categorySelected = (event,index)=>{
         const value = event.target.value;
         if(!isSelected(value)){
             const newArray = [...selectedCategories.current];
             const newData = [...data];
-            if(selectedCategories.length > 0 ){
+            if(selectedCategories.current.length > 0 ){
                 newArray.splice(index);
                 newData.splice(index+1);
             }
@@ -66,7 +70,7 @@ function SelectCategory({ formFields,setFormData}) {
         }else{
             setData(initialData);
         }
-    },[currentCategory,formFields.platform,getCategories])
+    },[currentCategory,formFields.platform,getCategories,setData])
 
     useEffect(()=>{
         const element = document.querySelector(`.category-list-container`);
